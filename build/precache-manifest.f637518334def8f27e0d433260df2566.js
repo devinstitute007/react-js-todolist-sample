661a2f745bb9ca2157cb301daa5ab3ee3764a59b
devinstitute007 <cjiogo@gmail.com>
2026-02-10 14:02:03 +0100

    react JS Todolist v1.0.0

---
+++ build/precache-manifest.f637518334def8f27e0d433260df2566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7bd4adbf8963489f052a93b538f4f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cdd53ee2904768a4b07",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fb99293a57e36660e9f9",
+    "url": "/static/css/main.c4055ae6.chunk.css"
+  },
+  {
+    "revision": "4cdd53ee2904768a4b07",
+    "url": "/static/js/2.4d9cc39c.chunk.js"
+  },
+  {
+    "revision": "fb99293a57e36660e9f9",
+    "url": "/static/js/main.8c1a2786.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
